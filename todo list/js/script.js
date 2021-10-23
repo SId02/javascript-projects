aa5560077767a97df3a9fa2a7f6d2336c048a5ae
Siddhesh M <31335947+SId02@users.noreply.github.com>
2021-10-23 10:45:04 +0530

    add todo

---
+++ todo list/js/script.js
@@ -0,0 +1,31 @@
+let form = document.querySelector("#addForm");
+let itemList = document.querySelector("#add_list");
+
+
+form.addEventListener("submit", addItem);
+// Delete event
+itemList.addEventListener("click", removeItem);
+
+
+function addItem(e) {
+	e.preventDefault();
+	// Get input value
+	let newItem = document.querySelector("#todo_insert").value;
+	let li = document.createElement("li");
+	li.className = "card-content";
+	li.appendChild(document.createTextNode(newItem));
+	let deleteBtn = document.createElement("button");
+	deleteBtn.className = "delete";
+	deleteBtn.appendChild(document.createTextNode("X"));
+	li.appendChild(deleteBtn);
+	itemList.appendChild(li);
+}
+
+// Remove item
+function removeItem(e){
+  if(e.target.classList.contains('delete')){
+      let li = e.target.parentElement;
+      itemList.removeChild(li);
+   
+  }
+}
