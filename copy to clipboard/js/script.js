bd6028f5ebac8236690a969305f2e897a523ae7d
Siddhesh M <31335947+SId02@users.noreply.github.com>
2021-12-16 11:52:26 +0530

    add

--- copy to clipboard/js/script.js
+++ copy to clipboard/js/script.js
@@ -1,11 +1,7 @@
-// document.querySelector("#generate").addEventListener("click", () => {
-//   generate();
-// });
 
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
-const btn = document.getElementById("button");
 const color = document.querySelector(".color");
-btn.addEventListener("click", function () {
+document.querySelector("#button").addEventListener("click", () => {
   let hexColor = "#";
   for (let i = 0; i < 6; i++) {
     hexColor += hex[getRandomNumber()];
@@ -21,12 +17,8 @@ function getRandomNumber() {
 
 
 function myFunction() {
-  /* Get the text field */
-  let copyText = document.getElementById("color-code");
-  console.log("copytext" + copyText);
-  copyText.select();
- // copyText.setSelectionRange(0, 99999); 
-  navigator.clipboard.write(copyText.value);
+  let copyText = document.querySelector("#color-code");
+  navigator.clipboard.writeText(copyText.value);
   alert("Copied the text: " + copyText.value);
 }
 
